b2724fed27cc9702aab42c47306972a4e81c02fe
Richard <richardchoi54@gmail.com>
2025-02-15 16:51:40 -0500

    add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header(){
+    return(
+        <header className="flex justify-end">
+            <nav>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,12 +3,14 @@ import { books } from "../static/books";
 import Button from "../components/Button";
 import { useState } from "react";
 import Footer from "../components/Footer";
+import Header from "../components/Header";
 
 export default function Home() {
   const [numOfItems, setNumOfItems] = useState<number>(5);
 
   return (
     <main className="p-2">
+      <Header/>
       <h1 className="text-5xl">Library</h1>
       <section className="flex items-center flex-col">
         {books
