f7213ab0e24c4d5e697d2c406d0a0aa7968db0b3
Richard <richardchoi54@gmail.com>
2025-02-15 14:57:57 -0500

    add bookcard component styling

--- src/components/BookCard.tsx
+++ src/components/BookCard.tsx
@@ -14,16 +14,20 @@ export default function BookCard({
   onClick: () => void;
 }) {
   return (
-    <section onClick={() => onClick()} className="flex">
+    <section onClick={() => onClick()} className="book-hover cursor-pointer shadow-[10px_10px_10px_#C9ABC5] min-w-[850px] flex items-center bg-[#F1B6CA] p-4 my-6 rounded-[10px]">
       <img className="w-50" src={book.image} alt={book.imageAlt} />
-      <section className="flex flex-col">
-      <h2>{book.name}</h2>
-      <h2>{book.author}</h2>
-      <div className="flex">
-      {book.genres.map((genre) => {
-        return <span key={genre}>{genre}</span>;
-      })}
-      </div>
+      <section className="flex flex-col p-4 items-start">
+        <h2 className="text-4xl mb-2">{book.name}</h2>
+        <h3 className="text-3xl">{book.author}</h3>
+        <div className="flex mt-6 items-center justify-between">
+          {book.genres.map((genre) => {
+            return (
+              <span className="text-base text-[#F1E4EE] p-2 bg-[#BC546B] rounded-[10px] mr-2" key={genre}>
+                {genre}
+              </span>
+            );
+          })}
+        </div>
       </section>
     </section>
   );
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -5,9 +5,11 @@ export default function Home() {
   return (
     <main>
       <h1>Home</h1>
+      <section className="flex items-center flex-col">
       {books.map((book: Book) => {
         return <BookCard key={book.name} book={book} onClick={() => {}} />;
       })}
+      </section>
     </main>
   );
 }
